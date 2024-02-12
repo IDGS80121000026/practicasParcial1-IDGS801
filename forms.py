0c5afb4bd86716be32052399fed94b44958593f9
IDGS80121000026 <kookie01jeon@gmail.com>
2024-02-12 17:52:36 -0600

    BEGP 12/02/2024

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from wtforms import Form
 from flask_wtf import FlaskForm
-from wtforms import StringField,TelField,IntegerField
+from wtforms import StringField,TelField,IntegerField,FloatField
 from wtforms import EmailField
 from wtforms.validators import DataRequired, Email
 
@@ -10,4 +10,12 @@ class UserForm(Form):
     distancia2= IntegerField('distancia2')
     distancia3= IntegerField('distancia3')
     distancia4= IntegerField('distancia4')
+    
+class Resis(Form):
+    color1= IntegerField('color1')
+    color2= IntegerField('color2')
+    color3= IntegerField('color3')
+    tolerancia= IntegerField('tolerancia')
+    maxim= StringField('maxim')
+    minm= StringField('minm')
     
--- main.py
+++ main.py
@@ -43,6 +43,151 @@ def prac():
     
     return render_template("practica.html",form=distancia_clase,disp=disp, distancia1=distancia1, distancia2=distancia2, distancia3=distancia3, distancia4=distancia4)
     
+@app.route("/resistencia",methods=['GET','POST'])
+def resistencia():
+    color1=""
+    color2=""
+    color3=""
+    tolerancia=""
+    total =""
+    maxim =""
+    minm =""
+    co1=''
+    co2=''
+    co3=''
+    tol=''
+    horale=''
+    horale2=''
+    horale3=''
+    tole=''
+    
+    resistencia_clase =forms.Resis(request.form)
+    if request.method=='POST':
+        color1 = resistencia_clase.color1.data
+        color2 = resistencia_clase.color2.data
+        color3 = resistencia_clase.color3.data
+        tolerancia = resistencia_clase.tolerancia.data
+        total = int(str(color1) + str(color2)) * color3
+        
+          
+            
+        if color1 == 0:
+            co1='Negro'
+            horale='#565f5b'
+        elif color1 == 1:
+            co1 ='Cafe'
+            horale='#a05b3d'
+        elif color1 == 2:
+            co1 ='Rojo'
+            horale='#ea1e1b'
+        elif color1 == 3:
+            co1 ='Naranja'
+            horale='#f2863e'
+        elif color1 == 4:
+            co1 ='Amarillo'
+            horale='#f2e23e'
+        elif color1 == 5:
+            co1 ='Verde'
+            horale='#bbe231'
+        elif color1 == 6:
+            co1 ='Azul'
+            horale='#71a4e2'
+        elif color1 == 7:
+            co1 ='Violeta'
+            horale='#a571e2'
+        elif color1 == 8:
+            co1 ='Gris'
+            horale='#dbd5e2'
+        else:
+            co1 ='Blanco'
+            horale='#fefbfb'
+            
+        if color2 == 0:
+            co2='Negro'
+            horale2='#565f5b'
+        elif color2 == 1:
+            co2 ='Cafe'
+            horale2='#a05b3d'
+        elif color2 == 2:
+            co2 ='Rojo'
+            horale2='#ea1e1b'
+        elif color2 == 3:
+            co2 ='Naranja'
+            horale2='#f2863e'
+        elif color2 == 4:
+            co2 ='Amarillo'
+            horale2='#f2e23e'
+        elif color2 == 5:
+            co2 ='Verde'
+            horale2='#bbe231'
+        elif color2 == 6:
+            co2 ='Azul'
+            horale2='#71a4e2'
+        elif color2 == 7:
+            co2 ='Violeta'
+            horale2='#a571e2'
+        elif color2 == 8:
+            co2 ='Gris'
+            horale2='#dbd5e2'
+        else:
+            co2 ='Blanco'
+            horale2='#fefbfb'
+        
+        if color3 == 1:
+            co3='Negro'
+            horale3='#565f5b'
+        elif color3 == 10:
+            co3 ='Cafe'
+            horale3='#a05b3d'
+        elif color3 == 100:
+            co3 ='Rojo'
+            horale3='#ea1e1b'
+        elif color3 == 1000:
+            co3 ='Naranja'
+            horale3='#f2863e'
+        elif color3 == 10000:
+            co3 ='Amarillo'
+            horale3='#f2e23e'
+        elif color3 == 100000:
+            co3 ='Verde'
+            horale3='#bbe231'
+        elif color3 == 1000000:
+            co3 ='Azul'
+            horale3='#71a4e2'
+        elif color3 == 1000000:
+            co3 ='Violeta'
+            horale3='#a571e2'
+        elif color3 == 100000000:
+            co3 ='Gris'
+            horale3='#dbd5e2'
+        else: 
+            co3 ='Blanco'
+            horale3='#fefbfb'
+            
+        if tolerancia == 5:
+            tol = 'Oro'
+            maxim = total+(total*.05)
+            minm = total-(total*.05)
+            tole='#cfc436'
+        elif tolerancia == 10:
+            tol= 'Plata'
+            maxim = total+(total*.10)
+            minm = total-(total*.10)
+            tole='#e4e4e4'
+        else :
+            tol='Quien sabe como le hicite'
+            
+        print('max {}'.format(maxim))    
+        print('min {}'.format(minm)) 
+    
+        
+        
+    print('dkd {}'.format(total))        
+    return render_template("resistencia.html",form=resistencia_clase,color1=color1, color2=color2, color3=color3, tolerancia=tolerancia, total=total,maxim=maxim, minm=minm, co1=co1,co2=co2,co3=co3,tol=tol,horale=horale, horale2=horale2,horale3=horale3,tole=tole)
+        
+        
+        
+    
         
 if __name__=="__main__":
     app.run(debug=True)
